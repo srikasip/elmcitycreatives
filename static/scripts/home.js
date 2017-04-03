554ce64405e8f464923f755ce5c4e23e9d626196
srikasip <srikasip@gmail.com>
2017-04-03 11:56:44 -0400

    Updated the site to be a little cleaner, removed a bunch of files, and made the setup more parallax friendly for later

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-  $("a.heroLink").hover(function(){
-    assoc_id = $(this).attr("data-partner");
-    $("#"+assoc_id).css("display", "block");
-    console.log(assoc_id);
-  }, function(){
-    $("a.linkPartner").css("display","none");
-  });
+  
 
 });
